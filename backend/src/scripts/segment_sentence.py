a20303d45ff98135b3e79d4d729dac5cdda05171
David Li <d.li.2247@gmail.com>
2020-11-28 23:39:37 -0800

    Pass text to script as arg

--- backend/src/scripts/segment_sentence.py
+++ backend/src/scripts/segment_sentence.py
@@ -1,3 +1,5 @@
+import getopt
+import sys
 import pysbd
 import string
 
@@ -9,14 +11,18 @@ def clean_sentence(sentence):
     return sentence.strip(strip_chars).lower()
 
 
-def main():
-    data = open('src/scripts/data.txt', 'r')
-    try:
-        sentences = segmenter.segment(data.read())
+def main(argv):
+    opts, args = getopt.getopt(argv, '')
+
+    if len(args) == 0:
+        print('No URL found', file=sys.stderr)
+    # data = open('src/scripts/data.txt', 'r')
+    # try:
+        sentences = segmenter.segment(args[0])
         cleaned_sentences = [clean_sentence(sentence) for sentence in sentences]
         print(cleaned_sentences)
-    finally:
-        data.close()
+    # finally:
+    #     data.close()
 
 
 if __name__ == "__main__":
